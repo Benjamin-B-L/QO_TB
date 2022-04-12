d45f105d5f56cfdd33922d476bf7c9cb5add30d5
Benjamin.Bacq-Labreuil <benjamin.bacq-labreuil@mba-10826152.local>
2022-04-12 12:11:36 +0200

    try for a fix still for Q vectors like 2*pi/3

--- src/dos_wmagfield.jl
+++ src/dos_wmagfield.jl
@@ -236,7 +236,7 @@ function diagblock(ix::Int64,B::Float64,theta::Float64,ky_list::Vector{Float64},
                             if abs(q[1])<1e-8
                                 #+Qy
                                 kq = mod((ky_list[ik]+q[2]),2pi)
-                                kq_index = cnt_perlay[ilay] + findfirst(x->abs(x-kq)<1e-8,ky_list)
+                                kq_index = cnt_perlay[ilay] + findfirst(x->abs(x-kq)<1e-4,ky_list)
                                 H[cnt,kq_index] += cdw_coupling("diag","y",ky_list[ik],Layer.Pcdw[ilay],ix,0.0,B,theta)
                                 H[kq_index,cnt] += cdw_coupling("diag","y",ky_list[ik],Layer.Pcdw[ilay],ix,0.0,B,theta)
                                 #-Qy
